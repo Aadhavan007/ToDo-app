12db22960c294426dfa40ec9bc2c4c54186a41eb
Aadhavan007 <aadhavannamuthukumar@gmail.com>
2026-07-31 10:59:43 +0530

    To-do Components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      My To-Do List App
     </div>
   );
 }
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,4 @@
+function ToDO() {
+    return(<>My To-Do List components</>)
+}
+export default ToDO;
